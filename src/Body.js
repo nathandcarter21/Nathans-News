3c7a065ccdc7af8c8577d99e9d3d748fa75f686c
Nathan Carter <97918203+nathandcarter21@users.noreply.github.com>
2022-06-30 20:28:30 -0700

    first data fetch

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,10 @@
-import { Card, Button, Form } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Load from "./Load";
 import Category from "./Category";
+const url =
+	"https://newsapi.org/v2/everything?q=business&apiKey=76a5bed9e5d34c449994a27be24bcfa7";
 
 const stock =
 	"https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV3c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60";
@@ -12,73 +15,74 @@ const Body = () => {
 		{
 			source: {
 				id: null,
-				name: "Tech Times",
+				name: "Gizmodo.com",
 			},
-			author: "Griffin Davis",
-			title:
-				"Tesla Cybertruck Inspired by James Bond Submarine Car? Here's What You Need To Know About 'Wet Nellie'",
-			description: 'Here\'s what you need to know about "Wet Nellie."',
-			url: "https://www.techtimes.com/articles/276895/20220619/tesla-cybertruck-inspired-james-bond-submarine-car-heres-what-need.htm",
+			author: "Shoshana Wodinsky",
+			title: "Google's Ad Business Could Finally Crack Open",
+			description:
+				"While Google’s multiple antitrust cases continue to drag on here in the U.S., it looks like the search giant’s starting to make a few concessions across the pond. Reuters reports that Google’s parent company, Alphabet, has made an offer to European Union regu…",
+			url: "https://gizmodo.com/google-youtube-eu-antitrust-ads-change-1849054734",
 			urlToImage:
-				"https://1734811051.rsc.cdn77.org/data/images/full/406780/tesla-cybertruck-inspired-by-james-bond-submarine-car-heres-what-you-need-to-know-about-wet-nellie.jpg",
-			publishedAt: "2022-06-19T20:40:00Z",
+				"https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/671dc6bff2ba77cbfc00fda39e7419be.jpg",
+			publishedAt: "2022-06-13T21:39:00Z",
 			content:
-				"The Tesla Cybertruck is one of the most popular inventions of Elon Musk. This zero-emission pickup truck made headlines after its iconic endurance test back in 2019.\r\nDuring this public demonstration… [+2373 chars]",
+				"While Googles multiple antitrust cases continue to drag on here in the U.S., it looks like the search giants starting to make a few concessions across the pond. Reuters reports that Googles parent co… [+3000 chars]",
 		},
 		{
 			source: {
-				id: null,
-				name: "MobileSyrup",
+				id: "engadget",
+				name: "Engadget",
 			},
-			author: "Karandeep Oberoi",
+			author: "Steve Dent",
 			title:
-				"13-year-old from Saskatoon among Apple’s Swift Student Challenge winners",
+				"Sony Honda Mobility Inc. is the new name of Sony and Honda's EV business",
 			description:
-				"Drake Jordan, who started coding just two years ago after being intrigued by Apple’s Worldwide Developer Conference naturally experiences synesthesia, a perception phenomenon where the stimulation of one sense involuntarily leads to the stimulation of another…",
-			url: "https://mobilesyrup.com/2022/06/19/13-year-old-from-saskatoon-among-apples-swift-student-challenge-winners/",
+				"After Sony and Honda announced plans to form a separate company for their joint electric vehicle partnership, they've now given it a name. Yes, the new business is called Sony Honda Mobility Inc. and will be established in Tokyo before the end of 2022, with E…",
+			url: "https://www.engadget.com/sony-and-hondas-independent-ev-business-is-called-sony-honda-mobility-inc-093538621.html",
 			urlToImage:
-				"https://cdn.mobilesyrup.com/wp-content/uploads/2022/06/Drake-Jordan-Tim-cook-header.jpg",
-			publishedAt: "2022-06-19T20:01:37Z",
+				"https://s.yimg.com/os/creatr-uploaded-images/2022-06/893cd810-ed52-11ec-b7ff-0b3dd3ab42b1",
+			publishedAt: "2022-06-16T09:35:38Z",
 			content:
-				"Apple hosts its Swift Student Challenge annually that tasks participating youngsters globally to display their coding skills by creating a Swift Playground app project on the topic of their choice.\r\n… [+1650 chars]",
+				"After Sony and Honda announced plans to form a separate company for their joint electric vehicle partnership, they've now given it a name. Yes, the new business is called Sony Honda Mobility Inc. and… [+1463 chars]",
 		},
 		{
 			source: {
-				id: null,
-				name: "The Daily Hodl",
+				id: "wired",
+				name: "Wired",
 			},
-			author: "Daily Hodl Staff",
+			author: "Chris Stokel-Walker",
 			title:
-				"These Three Catalysts Could Spark a Bitcoin (BTC) Reversal, According to Top Crypto Analyst",
+				"The Tricky Business of Elon Musk Getting Twitter Fire-Hose Access",
 			description:
-				"A closely followed crypto analyst is identifying three catalysts that could lead to a reversal of Bitcoin (BTC) as the benchmark cryptocurrency hovers below $20,000. In a new strategy session, the host of InvestAnswers tells his 441,000 YouTube subscribers th…",
-			url: "https://dailyhodl.com/2022/06/19/these-three-catalysts-could-spark-a-bitcoin-btc-reversal-according-to-top-crypto-analyst/",
+				"Twitter has reportedly given the billionaire access to its full stream of tweets and related user data. Is your privacy in jeopardy?",
+			url: "https://www.wired.com/story/elon-musk-twitter-firehose/",
 			urlToImage:
-				"https://dailyhodl.com/wp-content/uploads/2022/03/bitcoin-price-action.jpg",
-			publishedAt: "2022-06-19T20:00:43Z",
+				"https://media.wired.com/photos/62a393d78eaf9419b31098a5/191:100/w_1280,c_limit/musk_sec_GettyImages-1395062612.jpg",
+			publishedAt: "2022-06-10T19:01:24Z",
 			content:
-				"A closely followed crypto analyst is identifying three catalysts that could lead to a reversal of Bitcoin (BTC) as the benchmark cryptocurrency hovers below $20,000.\r\nIn a new strategy session, the h… [+1837 chars]",
+				"Elon Musks never-ending attempt to take over Twitter has taken yet another weird turn as the social media platform appears to have acceded to the entrepreneurs request to gain access to a fire hose o… [+3082 chars]",
 		},
 		{
 			source: {
-				id: null,
-				name: "CarScoops",
+				id: "wired",
+				name: "Wired",
 			},
-			author: "Brad Anderson",
-			title:
-				"These Are The Cars And Trucks With The Highest And Lowest Depreciation In The USA",
+			author: "Will Knight",
+			title: "China Built Your iPhone. Will It Build Your Next Car?",
 			description:
-				"The Toyota Tacoma only drops 21.93% in value in the first three years of ownership",
-			url: "https://www.carscoops.com/2022/06/toyota-tacoma-holds-its-value-best-in-the-u-s-while-kia-sorento-depreciates-the-most/",
+				"Gadget manufacturers are getting into the car-making business. That could shake up the auto industry, global trade, and geopolitics.",
+			url: "https://www.wired.com/story/foxconn-apple-car-china/",
 			urlToImage:
-				"https://www.carscoops.com/wp-content/uploads/2022/06/New-Car-Values-1a.jpg",
-			publishedAt: "2022-06-19T18:50:34Z",
+				"https://media.wired.com/photos/62abb1d61adf15cb4e383623/191:100/w_1280,c_limit/ChinaElectric-01.png",
+			publishedAt: "2022-06-27T11:00:00Z",
 			content:
-				"If youre shopping for a new vehicle in the U.S. that will hold its value well, buying a pickup truck could be the best option.\r\nAn analysis from Zutobi has revealed that the three vehicles which hold… [+2114 chars]",
+				"Rumors of an Apple electric car project have long excited investors and iPhone enthusiasts. Almost a decade after details of the project leaked, the Cupertino-mobile remains mythicalbut that hasnt st… [+2952 chars]",
 		},
 	]);
-	let [rendArticles, setRendArticles] = useState(articles);
-	let [url, setUrl] = useState("stonks");
+	let [rendArticles, setRendArticles] = useState([]);
+	// let [url, setUrl] = useState("stonks");
+	let [loading, setLoading] = useState(true);
+	let [error, setError] = useState(false);
 
 	const addNumArticles = () => {
 		if (articles.length > numArticles) {
@@ -86,7 +90,6 @@ const Body = () => {
 		}
 	};
 	const loadArticles = () => {
-		console.log("mefaisfhjdshfsh");
 		const res = articles.filter((article, index) => index < numArticles);
 		setRendArticles(res);
 	};
@@ -101,66 +104,97 @@ const Body = () => {
 		//loadArticles();wont need this because every time we change data we will also want to set num articles to 1 which will run the other use effect
 
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [url]);
+	}, [articles]);
 	useEffect(() => {
 		loadArticles();
 		//need one to only loadMore when the num changes and need the other use effect for when we need to load new data like when the url changes
 
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [numArticles]);
+	useEffect(() => {
+		// setLoading(true);
+		// axios
+		// 	.get(url)
+		// 	.then((response) => {
+		// 		console.log(response.data.articles);
+		// 		if (response.data.status !== "ok") {
+		// 			setError(true);
+		// 			throw Error();
+		// 		}
+		// 		setArticles(response.data.articles);
+		// 		setLoading(false);
+		// 	})
+		// 	.catch(() => {
+		// 		console.log("There was an error");
+		// 		setLoading(false);
+		// 		setError(true);
+		// 	});
+		console.log("fetching data");
+		setLoading(false);
+	}, []);
 
-	return (
-		<div className="body">
-			<div className="categories d-flex justify-content-evenly align-items-center flex-wrap ">
-				<Category category={"Business"} />
-				<Category category={"Finance"} />
-				<Category category={"Cars"} />
-				<Category category={"Econ"} />
-				<input type="text" placeholder="Search" className="search mt-4 px-3" />
-			</div>
-			<div className="news d-flex justify-content-around flex-wrap">
-				{rendArticles.map((article, index) => (
-					<Card className="cardd m-4">
-						<Card.Img variant="top" src={article.urlToImage || stock} />
-						<Card.Body className="d-flex">
-							<Card.Title>{article.title}</Card.Title>
-						</Card.Body>
-						<Card.Body className="d-flex">
-							<Card.Text>{article.content.slice(0, 200)}</Card.Text>
-						</Card.Body>
-						<Card.Body className="d-flex">
-							<Card.Link
-								href={article.url}
-								target="_blank"
-								className="btn btn-primary cardLink">
-								Go to article
-							</Card.Link>
-						</Card.Body>
-					</Card>
-				))}
-			</div>
-			<div className="text-center mb-4">
+	if (loading) {
+		return <h1>Is Loading...</h1>;
+	} else if (error) {
+		return <h1>Error</h1>;
+	} else {
+		return (
+			<div className="body">
+				<div className="categories d-flex justify-content-evenly align-items-center flex-wrap ">
+					<Category category={"Business"} />
+					<Category category={"Finance"} />
+					<Category category={"Crypto"} />
+					<Category category={"Science"} />
+					<input
+						type="text"
+						placeholder="Search"
+						className="search mt-4 mx-3"
+					/>
+				</div>
+				<div className="news d-flex justify-content-around flex-wrap">
+					{rendArticles.map((article, index) => (
+						<Card className="cardd m-4" key={index}>
+							<Card.Img variant="top" src={article.urlToImage || stock} />
+							<Card.Body className="d-flex">
+								<Card.Title>{article.title}</Card.Title>
+							</Card.Body>
+							<Card.Body className="d-flex">
+								<Card.Text>{article.content.slice(0, 200)}</Card.Text>
+							</Card.Body>
+							<Card.Body className="d-flex">
+								<Card.Link
+									href={article.url}
+									target="_blank"
+									className="btn btn-primary cardLink">
+									Go to article
+								</Card.Link>
+							</Card.Body>
+						</Card>
+					))}
+				</div>
+				<div className="text-center mb-4">
+					<Button
+						className=""
+						onClick={() => {
+							setNumArticles(1);
+						}}>
+						Back to 25
+					</Button>
+				</div>
+				<Load
+					addNumArticles={addNumArticles}
+					numArticles={numArticles}
+					totalArticles={articles.length}
+				/>
 				<Button
-					className=""
 					onClick={() => {
-						setNumArticles(1);
+						setLoading(true);
 					}}>
-					Back to 25
+					Blow up
 				</Button>
 			</div>
-			<Load
-				addNumArticles={addNumArticles}
-				numArticles={numArticles}
-				totalArticles={articles.length}
-			/>
-			<Button
-				onClick={() => {
-					setUrl("nah mate");
-				}}>
-				Blow up
-			</Button>
-		</div>
-	);
+		);
+	}
 };
 
 export default Body;
